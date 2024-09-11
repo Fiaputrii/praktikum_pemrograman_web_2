cd8d6b3d51bb27fd1547bcce4a092d16187253e4
Fiaputrii <165300379+Fiaputrii@users.noreply.github.com>
2024-09-11 23:30:26 +0700

    Update 3.encapsulation3.php

--- Jobsheet 3/3.encapsulation3.php
+++ Jobsheet 3/3.encapsulation3.php
@@ -27,9 +27,18 @@ class Student {
 
 $student1 = new Student();
 
-$student1->setName("Devia Herena");
-$student1->setStudentID("230302051");
+$student1->setName("Allina Naomi");
+$student1->setStudentID("230330112");
 
+echo "Nama: " . $student1->getName() . "<br>";
+echo "Student ID: " . $student1->getStudentID() . "<br>";
+
+// Mengubah nilai atribut menggunakan setter
+$student1->setName("Pamungkas");
+$student1->setStudentID("320202452");
+
+// Menampilkan data mahasiswa setelah perubahan
+echo "<br>Data mahasiswa setelah perubahan:<br>";
 echo "Nama: " . $student1->getName() . "<br>";
 echo "Student ID: " . $student1->getStudentID() . "<br>";
 ?>
